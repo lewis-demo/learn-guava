25ece9d002635d3bf31518a38fcb869bd0b0799d
lewis007 <lewis.lu.fs@icloud.com>
2016-01-28 17:33:26 +0800

    learn: commit cxf project

---
+++ learn-ws/soap-cxf/src/main/java/org/learn/soap/cxf/App.java
@@ -0,0 +1,13 @@
+package org.learn.soap.cxf;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
