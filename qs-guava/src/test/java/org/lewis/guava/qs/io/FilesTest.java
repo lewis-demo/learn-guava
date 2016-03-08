97f3355f4a326242b62b815f2467498cec6fcc17
lewis007 <lewis.lu.fs@icloud.com>
2016-03-08 23:56:47 +0800

    init qs-guava: Quick Start for Googel Guava

---
+++ qs-guava/src/test/java/org/lewis/guava/qs/io/FilesTest.java
@@ -0,0 +1,21 @@
+package org.lewis.guava.qs.io;
+
+import org.junit.Test;
+
+import com.google.common.io.Files;
+
+/**
+ * Test for Strings.
+ * 
+ * @author lewis007
+ * @since 0.1.0
+ * @see com.google.common.io.Files
+ */
+public class FilesTest {
+
+	@Test
+	public void test() {
+		Files.createTempDir();
+	}
+
+}
